08bec69ef1a0acf4ca3ab53edfebd3ba356de66e
HarryAtkin <Harry.atkinson2005@gmail.com>
2024-08-20 16:10:54 +0100

    Displaying/reading a get request

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listenting on port: " + server.getLocalPort());
         while (true) {
             final Socket client = server.accept();
-            /*
-            1. Read Http request from the client socket
-            2. Prepare an HTTP response
-            3. Send HTTP response to the client
-            4. Close the socket.
-             */
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
         }
     }
 }
